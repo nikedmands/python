9414060d16168da78c220fd1c19f1a2eb24c5420
Nik Edmands <nikedmands@gmail.com>
2023-06-19 23:04:49 +0100

    QA course

--- Practice/number_guess.py
+++ Practice/number_guess.py
@@ -6,29 +6,32 @@ num = random.randint(1, 101)
 max_guess = 10
 guess = 0
 
-# welcome line
-print('Welcome to the number guessing game. Please guess the number between 0 - 100')
-# start or while loop
-# run loop whilst number of users guess < 10
-while guess < max_guess:
-    # users guess
-    user_num = input('Please make a guess: ')
-    # add 1 to users guesses variable
-    guess += 1
-    # turn the string from input, into an integer
-    user_num = int(user_num)
-    # if the guess is less than num, print too low
-    if user_num < num:
-        print('Your guess was too low, please try again.')
-    # else if the guess is too high, print too high
-    elif user_num > num:
-        print('Your guess was too high, please try again.')
-    # else, if none of the above have been met, the guess must == num
-    else:
-        print('CORRECT!')
-        # print guess variable (counter)
-        print('It only took you', guess, 'attempt(s)!')
-        exit()
+x = 'y'
 
-# this will print it the while loop didnt run (i.e. guess no longer less than 10, max number of guesses allowed)
-print('Sorry, you reached the maximum number of attempts.')
+while x == 'y':
+    # welcome
+    print('Welcome to the number guessing game. Please guess the number between 0 - 100')
+    # start or while loop
+    # run loop whilst number of users guess < 10
+    while guess < max_guess:
+        # users guess
+        user_num = input('Please make a guess: ')
+        # add 1 to users guesses variable
+        guess += 1
+        # turn the string from input, into an integer
+        user_num = int(user_num)
+        # if the guess is less than num, print too low
+        if user_num < num:
+            print('Your guess was too low, please try again.')
+        # else if the guess is too high, print too high
+        elif user_num > num:
+            print('Your guess was too high, please try again.')
+        # else, if none of the above have been met, the guess must == num
+        else:
+            print('CORRECT!')
+            # print guess variable (counter)
+            print('It only took you', guess, 'attempt(s)!')
+            exit()
+# this will print it the while loop didn't run (i.e. guess no longer less than 10, max number of guesses allowed)
+print('Too many attempts. Goobye!')
+exit()
