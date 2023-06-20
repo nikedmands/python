bfae68dbb4ee4eb5dc2fbed744b1712035e284dd
Nik Edmands <nikedmands@gmail.com>
2023-06-20 19:21:26 +0100

    QA course

---
+++ Practice/random.py
@@ -0,0 +1,11 @@
+a = "Hello, World"
+print(len(a))
+txt = "My name is Nik"
+print("Nik" in txt)
+
+# string formatting
+quantity = 3
+item_no = 567
+price = 49.95
+my_order = "I want {} pieces of item {} for {} dollars."
+print(my_order.format(quantity, item_no, price))
